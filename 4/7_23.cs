0b17dc2b9da0c93b0db9ae47618332720918155d
NoAnnY <noanny@mail.ru>
2022-11-10 22:42:24 +0300

    решение

---
+++ 4/7_23.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Please, enter any number");
+int number = int.Parse(Console.ReadLine()!);
+if (number % 7 == 0 && number % 23 == 0) Console.WriteLine("Everything is OK");
+else Console.WriteLine("DAMAGE");
+
