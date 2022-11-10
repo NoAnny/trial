464bfb20e70d48b608cbd96b046704cc984b63f5
NoAnnY <noanny@mail.ru>
2022-11-10 22:40:50 +0300

    решение

---
+++ 3/weekend.cs
@@ -0,0 +1,11 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+bool DayWeekend(int day)
+{
+    return day > 5;
+}
+
+Console.WriteLine(" Сегодня выходной?");
+Console.WriteLine("Чтобы узнать, введите номер дня недели (от 1 до 7)"); 
+int num = int.Parse(Console.ReadLine()!); 
+Console.WriteLine(DayWeekend(num));
