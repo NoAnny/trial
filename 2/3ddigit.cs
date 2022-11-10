717a95c7d04fe75df682d8abf2b12ec548311a8a
NoAnnY <noanny@mail.ru>
2022-11-10 22:39:23 +0300

    решение

---
+++ 2/3ddigit.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int ThirdDigit(int number)
+    {
+        while (number > 999)
+         {
+            number = number / 10;
+         }
+         return number % 10;
+    }
+
+Console.WriteLine("Введите число");
+int usernumber = int.Parse(Console.ReadLine ()!);
+if (usernumber < 100) Console.WriteLine("У числа нет третьей цифры");
+else Console.WriteLine(ThirdDigit(usernumber));
