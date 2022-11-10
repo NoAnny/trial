b83bfdf962cd28a16126000b7598b26c46b35d5e
NoAnnY <noanny@mail.ru>
2022-11-10 22:37:20 +0300

    Решение

---
+++ 1/2ddigit.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+int SecondDigit(int number)
+{
+    return (number % 100) / 10;
+}
+Console.WriteLine("Введите трехзначное число");
+int usernumber = int.Parse(Console.ReadLine ()!);
+Console.WriteLine(SecondDigit(usernumber));
